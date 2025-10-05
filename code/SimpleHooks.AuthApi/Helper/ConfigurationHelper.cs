b197398e57917b4cffb159db48bc639df635dfd6
George Nairooze <george.nairooze@gmail.com>
2025-10-05 11:24:55 +0300

    enhance code

--- code/SimpleHooks.AuthApi/Helper/ConfigurationHelper.cs
+++ code/SimpleHooks.AuthApi/Helper/ConfigurationHelper.cs
@@ -6,55 +6,55 @@ namespace SimpleTools.SimpleHooks.AuthApi.Helper
     {
         public string ConnectionStringLog
         {
-            get => config.GetSection("connectionStrings")["log"];
+            get => config.GetSection("connectionStrings")["log"] ?? string.Empty;
             set => config.GetSection("connectionStrings")["log"] = value;
         }
 
         public string ConnectionStringSimpleHooks
         {
-            get => config.GetSection("connectionStrings")["simpleHooks"];
+            get => config.GetSection("connectionStrings")["simpleHooks"] ?? string.Empty;
             set => config.GetSection("connectionStrings")["simpleHooks"] = value;
         }
 
         public string LoggerMinLogLevel
         {
-            get => config.GetSection("logger")["min-log-level"];
+            get => config.GetSection("logger")["min-log-level"] ?? string.Empty;
             set => config.GetSection("logger")["min-log-level"] = value;
         }
 
         public string LoggerFunction
         {
-            get => config.GetSection("logger")["function"];
+            get => config.GetSection("logger")["function"] ?? string.Empty;
             set => config.GetSection("logger")["function"] = value;
         }
 
         public string IdentityServerAuthority
         {
-            get => config.GetSection("IdentityServer")["Authority"];
+            get => config.GetSection("IdentityServer")["Authority"] ?? string.Empty;
             set => config.GetSection("IdentityServer")["Authority"] = value;
         }
 
         public string IdentityServerAudience
         {
-            get => config.GetSection("IdentityServer")["Audience"];
+            get => config.GetSection("IdentityServer")["Audience"] ?? string.Empty;
             set => config.GetSection("IdentityServer")["Audience"] = value;
         }
 
         public string IdentityServerIntrospectionEndpoint 
         {
-            get => config.GetSection("IdentityServer")["IntrospectionEndpoint"];
+            get => config.GetSection("IdentityServer")["IntrospectionEndpoint"] ?? string.Empty;
             set => config.GetSection("IdentityServer")["IntrospectionEndpoint"] = value;
         }
 
         public string IdentityServerClientId
         {
-            get => config.GetSection("IdentityServer")["ClientId"];
+            get => config.GetSection("IdentityServer")["ClientId"] ?? string.Empty;
             set => config.GetSection("IdentityServer")["ClientId"] = value;
         }
 
         public string IdentityServerClientSecret
         {
-            get => config.GetSection("IdentityServer")["ClientSecret"];
+            get => config.GetSection("IdentityServer")["ClientSecret"] ?? string.Empty;
             set => config.GetSection("IdentityServer")["ClientSecret"] = value;
         }
     }
--- code/SimpleHooks.AuthApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleTools.SimpleHooks.AuthApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; init; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
